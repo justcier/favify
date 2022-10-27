81ec6022e60b2fff0ec86dc92c8d9fb64368fcfe
jcier <syrina02@gmail.com>
2022-10-27 14:02:36 +0200

    Added files structure and color tokens

---
+++ lib/style/color_tokens.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class ColorTokens {
+  static const Color primaryColor = Color(0xFF002E94);
+  static const Color secondaryColor = Color(0xFF083AA9);
+  static const Color backgroundColor = Color(0xFFFFE7CC);
+  static const Color darkBackgroundColor = Color(0xFFE1CEB5);
+}
