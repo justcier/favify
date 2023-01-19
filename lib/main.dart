1c8b491c502f417c5648e272dd7eca5f276f3b70
jcier <syrina02@gmail.com>
2023-01-19 11:55:34 +0100

    Added configuration for firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:favify/firebase_options.dart';
 import 'package:favify/services/injection_service.dart';
 import 'package:favify/services/navigation_service.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   configureInjector();
   runApp(MyApp());
 }
